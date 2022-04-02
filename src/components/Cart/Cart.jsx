1018c72a843dc6d79a98b17513df62136c25571c
Godoy Federico Martin <GodoyFedericoMartin@JohnDeere.com>
2022-04-02 12:58:31 -0300

    carrito

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,27 +1,70 @@
 import React from 'react'
 import { useCartContext } from '../../context/CartContext'
-import { Button } from 'react-bootstrap'
+import { Badge, Button, ListGroup } from 'react-bootstrap'
 
 function Cart() {
 
   const {cartList, emptyCart} = useCartContext()
+console.log(cartList)
+/*   const montoTotal = cartlist.map(total => 
+  return(
+    for(i = cartList[0].cantidad; i<total.length; i++){
+      console.log(total)
+})) */
+
+
+
   return (
 
     <div>
       {cartList.map(prod => 
-      <li key= {prod.id}>
-            Nombre: {prod.title}
-            <br />
-            Precio: {prod.price}                                 
-            <br />
-            Descripión: {prod.description}
-            <br />
-            Cantidad: {prod.cantidad}
-      </li>)}
+      <div>
+        <ListGroup className="warning" key= {prod.id} as="ol" >
+          <ListGroup.Item
+            as="li"
+            className="d-flex justify-content-between align-items-start warning">
+            <div className="ms-2 me-auto">
+              <div className="fw-bold">
+                <h1>{prod.title}</h1>
+              </div>
+              <h3>Precio:{`$${(prod.price * prod.cantidad)}.-`}</h3>
+            </div>
+            <Badge bg="primary" pill>
+              <h2>Unidades: {prod.cantidad}</h2>
+            </Badge>
+          </ListGroup.Item>
+        </ListGroup>
+
+        <div>
+          <h1>Total de la compra: {prod.cantidad}</h1>
+        </div>
+      </div>)}
+
       <br/>
       <Button onClick={emptyCart}>Vaciar Cart</Button>
     </div>
   )
 }
 
+
+
 export default Cart
+
+/* {cartList.map(prod => 
+  <div>
+    <ListGroup key= {prod.id} as="ol" numbered>
+      <ListGroup.Item
+        as="li"
+        className="d-flex justify-content-between align-items-start">
+        <div className="ms-2 me-auto">
+          <div className="fw-bold">{prod.title}</div>
+          <h3>Precio: {prod.price}</h3>
+        </div>
+        <Badge bg="primary" pill>
+          {prod.cantidad}
+        </Badge>
+      </ListGroup.Item>
+    </ListGroup>
+    <br/>
+    <Button onClick={emptyCart}>Vaciar Cart</Button>
+  </div> */
--- src/components/NavBar/Widget/CartWidget.jsx
+++ src/components/NavBar/Widget/CartWidget.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
+import cart from '../../../imgs/cart.png'
 
-
-function Widget() {
+function CartWidget() {
   return (
     <>
         <img
              //className="d-block w-100"
-             src="https://cdn.icon-icons.com/icons2/1097/PNG/512/1485477061-cart_78585.png"
+             src= {`${cart}`}
              alt="carrito"
              style={{ height:'42px', width: '42px' }}
 
@@ -15,4 +15,4 @@ function Widget() {
   )
 }
 
-export default Widget
+export default CartWidget
